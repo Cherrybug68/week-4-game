29dca9aa5a3b1f6a9253b3801eb15beb0d1ca494
Shari Smith <Cherrybug68@gmail.com>
2016-10-17 15:38:02 -0400

    Mon PM Work

---
+++ assets/javascript/game.js
@@ -0,0 +1,78 @@
+// Variables
+var winCounter = 0;
+var loosCounter = 0;
+// var characterList = [knights, bulls, gators, noles];
+var randomHPNumb = [];
+
+
+
+createHP();
+attackPower();
+counterPower();
+
+
+// Create Health Points for each Character
+
+function createHP () {
+	knightsHP = Math.floor(Math.random()* 201 + 1);
+	$("#knightsHP").html(knightsHP);
+
+	bullsHP = Math.floor(Math.random()* 201 + 1);
+	$("#bullsHP").html(bullsHP);
+
+	gatorsHP = Math.floor(Math.random()* 201 + 1);
+	$("#gatorsHP").html(gatorsHP);
+
+	nolesHP = Math.floor(Math.random()* 201 + 1);
+	$("#nolesHP").html(nolesHP);
+}
+
+function attackPower () {
+	knightsAttack = Math.floor(Math.random()* 20 + 1);
+	$("#knightsAttack").html(knightsAttack);
+	console.log(knightsAttack);
+
+	bullsAttack = Math.floor(Math.random()* 20 + 1);
+	$("#bullsAttack").html(bullsAttack);
+	console.log(bullsAttack);
+
+	gatorsAttack = Math.floor(Math.random()* 20 + 1);
+	$("#gatorsAttack").html(gatorsAttack);
+	console.log(gatorsAttack);
+
+	nolesAttack = Math.floor(Math.random()* 20 + 1);
+	$("#nolesAttack").html(nolesAttack);
+	console.log(nolesAttack);
+}
+
+function counterPower () {
+	knightsCounterAttack = Math.floor(Math.random()* 20 + 1);
+	$("#knightsCounterAttack").html(knightsCounterAttack);
+	console.log(knightsCounterAttack);
+
+	bullsCounterAttack = Math.floor(Math.random()* 20 + 1);
+	$("#bullsCounterAttack").html(bullsCounterAttack);
+	console.log(bullsCounterAttack);
+
+	gatorsCounterAttack = Math.floor(Math.random()* 20 + 1);
+	$("#gatorsCounterAttack").html(gatorsCounterAttack);
+	console.log(gatorsCounterAttack);
+
+	nolesCounterAttack = Math.floor(Math.random()* 20 + 1);
+	$("#nolesCounterAttack").html(nolesCounterAttack);
+	console.log(nolesCounterAttack);
+}
+
+
+
+
+
+function yourCharacterMove(selectedElement) {
+	$(".yourCharacterSpot").append(selectedElement);
+}
+
+$(".character").on('click', function() {
+	var selectedElement = $(this);
+	yourCharacterMove(selectedElement);
+});
+
